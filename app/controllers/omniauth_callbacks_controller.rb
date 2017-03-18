ace4546e473c9b38157db860c625266707eb7072
mogura <ogura@localhost.localdomain>
2017-03-19 01:17:57 +0900

    omniauthのコントローラ、userのモデルに変更を加えて、omniauthの情報から自動で適当なemailとpasswordを生成、ユーザ登録と認証を行うように設定。 さらにviewで単にフォームを丸ごと消すことでユーザ登録をtwitter認証のみにすることができた 他に編集するような情報もないのでeditページからも消している ただし、usernameカラムの変更に対応するために後々その部分だけフォームタグを設置する可能性あり（認証時のユーザー名を取得してその後は変更されないため） Userモデルで:recoverableを消して、パスワード再設定のリンクも見えなくした

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
-    @user = User.from_omniauth(request.env['omniauth.auth'].except('extra'))
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
     
     # 登録済ならログイン、未登録なら新規登録画面へ
     if @user.persisted?
       sign_in_and_redirect @user
     else
-      session['devise.user_attributes'] = @user.attributes
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
