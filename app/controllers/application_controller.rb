8786785c34173f45da3db903478551035dded8d3
Masayoshi Ogura <jas@MasayoshinoMacBook-Pro.local>
2017-08-17 03:40:40 +0900

    .rubocop.yml作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-  
+
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
+
   private
+
   def sign_in_required
     redirect_to new_user_session_url unless user_signed_in?
   end
-  
+
   def approved_user!
-    redirect_to root_path, alert: '承認ユーザ用ページになります。\n承認希望の場合は管理者(@justice_vsbr)に連絡してください' unless current_user.approved?
+    return if curren_user.approved?
+
+    redirect_to root_path, alert: '承認ユーザ用ページになります。\n承認希望の場合は管理者(@justice_vsbr)に連絡してください'
   end
-  
+
   def admin_user!
-    redirect_to root_path, alert: '管理画面です\n一般ユーザは入れません' unless current_user.admin?
+    return if curren_user.admin?
+
+    redirect_to root_path, alert: '管理画面です\n一般ユーザは入れません'
   end
 end
